b95db35cc51e12fe54f55facfa7e3688398f4574
pearl <610999886@qq.com>
2026-06-17 19:20:12 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yzkongjian.glaze.fashion',
-  base: '/yzkongjian/',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
